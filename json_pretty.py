034e9b05e813e8a74c40e612168155e9e9e4b464
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2023-07-01 14:47:43 +0200

    Adding max_depth

--- json_pretty.py
+++ json_pretty.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
 
     ap = ArgumentParser(description='Pretty print JSON files + select just random examples')
     ap.add_argument('--select', '-s', type=int, help='Select N random examples')
+    ap.add_argument('--indent', '-i', type=int, help='Indent width', default=4)
+    ap.add_argument('--max-depth', '-m', type=int, help='Max depth for indentation', default=0)
     ap.add_argument('input_file', nargs='?', type=str, default='-', help='Input file (defaults to stdin)')
     args = ap.parse_args()
 
@@ -25,4 +27,15 @@ if __name__ == '__main__':
     if args.select and isinstance(data, list):
         data = random.sample(data, args.select)
 
-    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
+    lines = json.dumps(data, sort_keys=True, indent=args.indent, ensure_ascii=False).split('\n')
+    out = []
+    if args.max_depth:
+        for line in lines:
+            if line.startswith((args.max_depth * args.indent) * ' ') or line.startswith(((args.max_depth - 1) * args.indent) * ' ') and line.strip() in ['}', '},', '],', ']']:
+                out[-1] = out[-1] + line.lstrip()
+            else:
+                out.append(line)
+    else:
+        out = lines
+
+    print('\n'.join(out))
